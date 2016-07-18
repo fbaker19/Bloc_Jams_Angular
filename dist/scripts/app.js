27d3633b54dfd503fed68dfa727f046af76ed6bf
Fairen <fairen.baker@gmail.com>
2016-07-18 16:43:06 -0400

    angular sec 3

--- dist/scripts/app.js
+++ dist/scripts/app.js
@@ -1,1 +1,27 @@
-angular.module('blocJams',[]);
+(function(){
+    function config($stateProvider, $locationProvider){
+        $locationProvider
+            .html5Mode({
+                enabled:true,
+                requireBase:false
+                //enabled:true - disables hashbang mode ex localhost:3000/#!/album
+            });
+        
+        $stateProvider
+            .state('landing',{
+                url:'/',
+                templatUrl:'../templates/landing.html'
+            })
+
+            .state('album',{
+                url:'/album',
+                templateUrl:'../templates/album.html'
+            });
+    
+    }
+    
+    angular
+    .module('blocJams',['ui.router'])
+    .config(config);
+    
+})();
