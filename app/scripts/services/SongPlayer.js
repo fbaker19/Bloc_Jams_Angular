a0bbf20cf8942279043720232803176f9ad6686c
fbaker <fairen.baker@gmail.com>
2016-09-09 12:53:33 -0500

    play pause service

--- app/scripts/services/SongPlayer.js
+++ app/scripts/services/SongPlayer.js
@@ -3,31 +3,48 @@
         var SongPlayer = {};
         
         var currentSong = null;
+        
+        /*@desc: Buzz object audio file
+          @type: {Object}
+        */
         var currentBuzzObject = null;
         
-        SongPlayer.play = function(songs){
-            if(currentSong !== songs){
-                if(currentBuzzObject){
-                    currentBuzzObject.stop();
-                    currentSong.playing = null;
-                }
+        /*
+            @function: setSong
+            @desc: stops currently playing song and loads new audio file as currentBuzzObject
+            @param: {object} songs
+        */
+        var setSong = function(songs){
+            if(currentBuzzObject){
+                currentBuzzObject.stop();
+                currentSong.playing = null;
             }
+            
             currentBuzzObject = new buzz.sound(songs.audioUrl, {
                 formats:['mp3'],
                 preload:true
             });
             
             currentSong = songs;
-            
-            currentBuzzObject.play();
-            songs.playing = true;// referenced in album.html
+        };
+        
+        SongPlayer.play = function(songs){
+            if(currentSong !== songs){
+                setSong(songs);
+                currentBuzzObject.play();
+                songs.playing = true;// referenced in album.html
+            }else if(currentSong === songs){
+                if(currentBuzzObject.isPaused()){
+                    currentBuzzObject.play();
+                }
+            }
         };
         
         SongPlayer.pause = function(songs){
+            
             currentBuzzObject.pause();
             songs.playing = false;
             currentSong.playing = null;
-    
         };
         
         return SongPlayer;
